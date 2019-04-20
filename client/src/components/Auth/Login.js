5420b52034da0b102f627285f9d60fe062d5f400
Ajinkya Borade <ajinkyab1987@gmail.com>
2019-04-20 20:02:00 +0530

    Login component style fix

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -2,38 +2,56 @@ import React, { useContext } from "react";
 import { GoogleLogin} from "react-google-login";
 import { GraphQLClient } from "graphql-request";
 import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
 import Context from "../../store/context";
 import { LOGIN_USER } from "../../store/reducer";
-// import Typography from "@material-ui/core/Typography";
+import {ME_QUERY} from "../../graphql/queries";
 
-const ME_QUERY = `
-  {
-    me {
-      name
-      email
-      picture
-    }
-  }
-`;
 
 const Login = ({ classes }) => {
   const {dispatch} = useContext(Context);
 
-  const onSuccess = async (googleUser) => {
-    const idToken = googleUser.getAuthResponse().id_token;
-    
-    const client = new GraphQLClient("http://localhost:4000/graphql", {
-      headers: {authorization: idToken}
-    });
+  async function onSuccess (googleUser) {
+    try {
+      const idToken = googleUser.getAuthResponse().id_token;
+      const client = new GraphQLClient("http://localhost:4000/graphql", {
+        headers: {authorization: idToken}
+      });
 
-    const data = await client.request(ME_QUERY);
-    dispatch({type: LOGIN_USER, payload: data.me});
-  }; 
-  return <GoogleLogin 
-    clientId="156992429844-88tkja5l3l01meg1upcpf0u6i7nc5kct.apps.googleusercontent.com"
-    onSuccess={onSuccess}
-    isSignedIn={true}
-    />;
+      const data = await client.request(ME_QUERY);
+      dispatch({type: LOGIN_USER, payload: data.me});
+    } catch (err) {
+      onFailure(err);
+    }
+  };
+
+  function onFailure(err) {
+    console.error("Error GoogleLogin: ", err);
+  }
+
+  return (
+    <section className={classes.root}>
+      <Typography
+        component="h1"
+        variant="h3"
+        gutterBottom
+        noWrap
+        style={
+          {color: "rgb(66, 133, 244)"}
+        }
+      >
+        Welcome to Mapbox Hooks
+      </Typography>
+      <GoogleLogin
+        clientId="156992429844-88tkja5l3l01meg1upcpf0u6i7nc5kct.apps.googleusercontent.com"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        isSignedIn={true}
+        theme="dark"
+      />
+    </section>
+  );
 };
 
 const styles = {
