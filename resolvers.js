5c5d8c92558b53949c7d26b504536f6bb4c60dd7
Ajinkya Borade <ajinkyab1987@gmail.com>
2019-04-27 15:08:00 +0530

    CreatePin typeDefs

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,5 @@
 const { AuthenticationError } = require("apollo-server");
+const Pin = require("./models/Pin");
 
 const authenticated = (next) => (root, args, ctx, info) => {
     if(!ctx) {
@@ -11,5 +12,15 @@ const authenticated = (next) => (root, args, ctx, info) => {
 module.exports = {
     Query: {
         me: authenticated((root, args, ctx) => ctx.currentUser)
+    },
+    Mutation: {
+        createPin: authenticated(async (root, args, ctx) => {
+            const _Pin = await new Pin({
+                ...args.input,
+                author: ctx.currentUser._id
+            }).save();
+            const pinAdded = await Pin.populate(_Pin, "author");
+            return pinAdded;
+        })
     }
 }
